769128c1099e3a19a938fb0081c9559fe807dcd7
garadraw <garadraw@gmail.com>
2021-01-26 18:56:41 +0200

    First_Commit

---
+++ ValentinS/ShellWaveArgcArgv.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[]){
+
+	int i;
+
+	printf("%i\n", argc);
+	
+	
+	for (i = 0; i < argc; i++){
+		
+		printf("%s\n", argv[i]);
+
+	}
+
+
+
+	return 0;
+
+
+
+
+
+
+
+
+
+}
